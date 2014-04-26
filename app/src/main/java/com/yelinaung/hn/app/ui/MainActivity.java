d05b3ba2921b619212fbd44ca4ea9938c41510a3
Ye Lin Aung <me@yelinaung.com>
2014-04-26 19:37:40 +0630

    If you click an item, it opens in your browser. Simple as that.

--- app/src/main/java/com/yelinaung/hn/app/ui/MainActivity.java
+++ app/src/main/java/com/yelinaung/hn/app/ui/MainActivity.java
@@ -1,9 +1,29 @@
+/*
+ * Copyright 2014 Ye Lin Aung
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.yelinaung.hn.app.ui;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 import butterknife.ButterKnife;
@@ -51,6 +71,14 @@ public class MainActivity extends Activity {
     mSwipeView.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
       @Override public void onRefresh() {
         loadNews();
+      }
+    });
+
+    mNewsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+      @Override public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Intent browserIntent =
+            new Intent(Intent.ACTION_VIEW, Uri.parse(stories.get(position).link));
+        startActivity(browserIntent);
       }
     });
   }
